6d575e93ef8487bfaf69fa25e6be9c8778f26801
Yaroslav Furman <yaro330@gmail.com>
2020-05-16 21:26:56 +0300

    sched/tune: remove WALT support from stune assist
    We fully switched to PELT.
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -697,30 +697,6 @@ boost_write(struct cgroup_subsys_state *css, struct cftype *cft,
 	return 0;
 }
 
-#ifdef CONFIG_SCHED_WALT
-static int sched_boost_override_write_wrapper(struct cgroup_subsys_state *css,
-					      struct cftype *cft, u64 override)
-{
-#ifdef CONFIG_STUNE_ASSIST
-	if (task_is_booster(current))
-		return 0;
-#endif
-
-	return sched_boost_override_write(css, cft, override);
-}
-
-static int sched_colocate_write_wrapper(struct cgroup_subsys_state *css,
-					struct cftype *cft, u64 colocate)
-{
-#ifdef CONFIG_STUNE_ASSIST
-	if (task_is_booster(current))
-		return 0;
-#endif
-
-	return sched_colocate_write(css, cft, colocate);
-}
-#endif /* CONFIG_SCHED_WALT */
-
 static int boost_write_wrapper(struct cgroup_subsys_state *css,
 			       struct cftype *cft, s64 boost)
 {
@@ -804,13 +780,13 @@ static struct cftype files[] = {
 	{
 		.name = "sched_boost_no_override",
 		.read_u64 = sched_boost_override_read,
-		.write_u64 = sched_boost_override_write_wrapper,
+		.write_u64 = sched_boost_override_write,
 	},
 #ifdef CONFIG_SCHED_WALT
 	{
 		.name = "colocate",
 		.read_u64 = sched_colocate_read,
-		.write_u64 = sched_colocate_write_wrapper,
+		.write_u64 = sched_colocate_write,
 	},
 #endif
 	{
@@ -862,18 +838,16 @@ struct st_data {
 	char *name;
 	int boost;
 	bool prefer_idle;
-	bool colocate;
-	bool no_override;
 };
 
 static void write_default_values(struct cgroup_subsys_state *css)
 {
 	static struct st_data st_targets[] = {
-		{ "audio-app",	0, 0, 0, 0 },
-		{ "background",	0, 0, 0, 0 },
-		{ "foreground",	1, 0, 0, 1 },
-		{ "rt",		0, 0, 0, 0 },
-		{ "top-app",	1, 1, 0, 1 },
+		{ "audio-app",	0, 0},
+		{ "background",	0, 0},
+		{ "foreground",	1, 0},
+		{ "rt",		0, 0},
+		{ "top-app",	1, 1},
 	};
 	int i;
 
@@ -882,15 +856,10 @@ static void write_default_values(struct cgroup_subsys_state *css)
 
 		if (!strcmp(css->cgroup->kn->name, tgt.name)) {
 			pr_info("stune_assist: setting values for %s: boost=%d prefer_idle=%d colocate=%d no_override=%d\n",
-				tgt.name, tgt.boost, tgt.prefer_idle,
-				tgt.colocate, tgt.no_override);
+				tgt.name, tgt.boost, tgt.prefer_idle);
 
 			boost_write(css, NULL, tgt.boost);
 			prefer_idle_write(css, NULL, tgt.prefer_idle);
-#ifdef CONFIG_SCHED_WALT
-			sched_colocate_write(css, NULL, tgt.colocate);
-			sched_boost_override_write(css, NULL, tgt.no_override);
-#endif
 		}
 	}
 }
