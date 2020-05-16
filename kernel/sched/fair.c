987ca9d75bc29698b4571a3841c22d9977c3910b
Zachariah Kennedy <zkennedy87@gmail.com>
2020-05-16 18:28:48 +0500

    sched/fair.c: split sched_boost from walt dependant code
    This allows us to leverage CAF's powerhal while !CONFIG_SCHED_WALT.
    
    Signed-off-by: dracarys18 <karthihegde010@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -197,12 +197,11 @@ unsigned int sched_capacity_margin_up[NR_CPUS] = {
 unsigned int sched_capacity_margin_down[NR_CPUS] = {
 			[0 ... NR_CPUS-1] = 1280}; /* ~20% margin */
 
-#ifdef CONFIG_SCHED_WALT
 /* 1ms default for 20ms window size scaled to 1024 */
 unsigned int sysctl_sched_min_task_util_for_boost = 51;
 /* 0.68ms default for 20ms window size scaled to 1024 */
 unsigned int sysctl_sched_min_task_util_for_colocation = 35;
-#endif
+
 static unsigned int __maybe_unused sched_small_task_threshold = 102;
 
 static inline void update_load_add(struct load_weight *lw, unsigned long inc)
@@ -9451,11 +9450,6 @@ redo:
 
 		continue;
 next:
-#ifdef CONFIG_SCHED_WALT
-		trace_sched_load_balance_skip_tasks(env->src_cpu, env->dst_cpu,
-				env->src_grp_type, p->pid, load, task_util(p),
-				cpumask_bits(&p->cpus_allowed)[0]);
-#endif
 		list_move_tail(&p->se.group_node, tasks);
 	}
 
