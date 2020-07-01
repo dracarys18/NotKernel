db00193423aab7e8b6d7ebf7b788dde8684729c8
dracarys18 <karthihegde010@gmail.com>
2020-07-01 18:02:43 +0500

    printk: Dont print init related logs
    Due to some fuckups from ROM side now the userspace spams my dmesg with following logs so ignore all init messages
    once for all This is annoying as hell.
    
    init: Could not find 'vendor.xiaomi.hardware.citsensorservice@1.1::ICitSensorService/default' for ctl.interface_start
    init: Could not find 'vendor.xiaomi.hardware.citsensorservice@1.1::ICitSensorService/default' for ctl.interface_start
    
    Signed-off-by: dracarys18 <karthihegde010@gmail.com>

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -813,6 +813,11 @@ static ssize_t devkmsg_write(struct kiocb *iocb, struct iov_iter *from)
 			endp++;
 			len -= endp - line;
 			line = endp;
+			if (strstr(line,"init"))
+			{
+			   kfree(buf);
+			   return ret;
+			}
 		}
 	}
 
